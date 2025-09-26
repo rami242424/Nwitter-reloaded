e22e8f028000bdeca6245d6b939ea2aa5dae50c1
Rami <138556019+rami242424@users.noreply.github.com>
2025-09-26 10:00:00 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./components/layout";
 
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Layout />,
+    children: [
+      {
+        
+      }
+    ]
+  }
+]);
 
 function App() {
-
   return (
     <></>
   );

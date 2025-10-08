2d71afb08994f71707a02a9813984af29b6e24db
Rami <138556019+rami242424@users.noreply.github.com>
2025-10-08 10:00:00 +0900

    npm i firebase

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCuOuwOmH0hFHMvOxhhaztNcuZuiIaDjgI",
+  authDomain: "nwitter-reloaded-8504c.firebaseapp.com",
+  projectId: "nwitter-reloaded-8504c",
+  storageBucket: "nwitter-reloaded-8504c.firebasestorage.app",
+  messagingSenderId: "1017444137469",
+  appId: "1:1017444137469:web:091da1c51f2b4a43d74194"
+};
+
+const app = initializeApp(firebaseConfig);

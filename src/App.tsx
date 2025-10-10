08b2cbe12701556c1131c54751ef4470dc06534b
Rami <138556019+rami242424@users.noreply.github.com>
2025-10-10 10:00:00 +0900

    authStateReady

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 import { useEffect, useState } from "react";
 import LoadingScreen from "./components/loading-screen";
+import { auth } from "./firebase";
 
 const GlobalStyles = createGlobalStyle`
   ${reset};
@@ -49,8 +50,7 @@ const router = createBrowserRouter([
 function App() {
   const [isLoading, setLoading] = useState(true);
   const init = async() => {
-    // wait for firebase(while firebase is checking whether login or not & who is user)
-    //setTimeout(() => setIsLoading(false), 2000);
+    await auth.authStateReady();
     setLoading(false);
   }
   useEffect(() => {

64ca360eb97a7482fcc6d086d892057825a0d22b
Rami <138556019+rami242424@users.noreply.github.com>
2025-10-11 10:00:00 +0900

    create-account css

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,3 +1,21 @@
+import styled from "styled-components"
+
+const Wrapper = styled.div``;
+
+const Form = styled.form``;
+
+const Input = styled.input``;
+
+
 export default function CreateAccount(){
-    return <h1>create account</h1>;
+    return (
+        <Wrapper>
+            <Form>
+                <Input name="name" placeholder="Name" type="text" required/>
+                <Input name="email" placeholder="Email" type="email" required/>
+                <Input name="password" placeholder="Password" type="password" required/>
+                <Input type="submit" value="Create Account" />
+            </Form>
+        </Wrapper>
+    )
 }

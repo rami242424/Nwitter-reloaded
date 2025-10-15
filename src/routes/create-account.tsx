2b958b9b7e3c6c06f059628ae8c7832d54a22f2c
Rami <138556019+rami242424@users.noreply.github.com>
2025-10-15 10:00:00 +0900

    create-account css

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,11 +1,40 @@
 import React, { useState } from "react";
 import styled from "styled-components"
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 420px;
+    padding: 50px 0px;
+`;
 
-const Form = styled.form``;
+const Title = styled.h1`
+    font-size: 42px;
+`;
 
-const Input = styled.input``;
+const Form = styled.form`
+    margin-top: 50px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    width: 100%;
+`;
+
+const Input = styled.input`
+    padding: 10px 20px;
+    border-radius: 50px;
+    border: none;
+    width: 100%;
+    font-size: 16px;
+    &[type="submit"]{
+        cursor: pointer;
+        &:hover {
+            opacity: 0.8;
+        }
+    }
+`;
 
 
 export default function CreateAccount(){
@@ -13,6 +42,7 @@ export default function CreateAccount(){
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const {target : {name, value}} = e;
         if(name === "name"){
@@ -25,15 +55,21 @@ export default function CreateAccount(){
     }
     const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log(name, email, password);
+        //console.log(name, email, password);
+        try{
+        } catch (e) {
+        } finally {
+            setLoading(false);
+        }
     }
     return (
         <Wrapper>
+            <Title>Log into X</Title>
             <Form onSubmit={onSubmit}>
                 <Input onChange={onChange} name="name" value={name} placeholder="Name" type="text" required/>
                 <Input onChange={onChange} name="email" value={email} placeholder="Email" type="email" required/>
                 <Input onChange={onChange} name="password" value={password} placeholder="Password" type="password" required/>
-                <Input type="submit" value="Create Account" />
+                <Input type="submit" value={isLoading ? "Loading..." : "Create Account"} />
             </Form>
         </Wrapper>
     )

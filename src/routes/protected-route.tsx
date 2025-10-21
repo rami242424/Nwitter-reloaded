7dfccdef55581bf679a4f7129ce9f7529d0c3198
Rami <138556019+rami242424@users.noreply.github.com>
2025-10-21 10:00:00 +0900

    protected-route

---
+++ src/routes/protected-route.tsx
@@ -0,0 +1,12 @@
+import { Navigate } from "react-router-dom";
+import { auth } from "../firebase";
+
+//로그인한 경우만 보임 & 아닐시 로그인 또는 계정생성 페이지로 리디렉션
+export default function ProtectedRoute({children}: {children:React.ReactNode}){
+    
+    const user = auth.currentUser;
+    if(!user){
+        return <Navigate to="login" />;
+    }
+    return children;
+}

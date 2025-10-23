3f9a514ad8d8303fd0e12202b45af16e19a79635
Rami <138556019+rami242424@users.noreply.github.com>
2025-10-23 10:00:00 +0900

    protected-route : children

--- src/routes/protected-route.tsx
+++ src/routes/protected-route.tsx
@@ -5,7 +5,7 @@ import { auth } from "../firebase";
 export default function ProtectedRoute({children}: {children:React.ReactNode}){
     
     const user = auth.currentUser;
-    if(!user){
+    if(user === null){
         return <Navigate to="login" />;
     }
     return children;

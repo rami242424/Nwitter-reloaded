9ad9610f8bccf412711c385c212497bd092c0a70
Rami <138556019+rami242424@users.noreply.github.com>
2025-11-08 10:00:00 +0900

    Navigation Bar

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,9 +1,24 @@
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+const Wrapper = styled.div``;
+
+const Menu = styled.div``;
+
+const MenuItem = styled.div``;
 
 export default function Layout(){
     return(
-        <>
+        <Wrapper>
+            <Menu>
+                <MenuItem>
+                </MenuItem>
+                <MenuItem>
+                </MenuItem>
+                <MenuItem>
+                </MenuItem>
+            </Menu>
             <Outlet/>
-        </>
+        </Wrapper>
     )
 }

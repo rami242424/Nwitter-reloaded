21be3d7cd94d6f03f337fc7a66c22b76b8f29021
Rami <138556019+rami242424@users.noreply.github.com>
2025-11-10 10:00:00 +0900

    Navigation Bar

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,11 +1,44 @@
 import { Link, Outlet } from "react-router-dom";
 import styled from "styled-components";
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+    display: grid;
+    gap: 20px;
+    grid-template-columns: 1fr 4fr;
+    height: 100%;
+    padding: 50px 0px;
+    width: 100%;
+    max-width: 860px;
 
-const Menu = styled.div``;
+`;
 
-const MenuItem = styled.div``;
+const Menu = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+`;
+
+const MenuItem = styled.div`
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 2px solid white;
+    height: 50px;
+    width: 50px;
+    border-radius: 50%;
+    svg {
+        width: 30px;
+        fill: white;
+    }
+    &.log-out {
+        border-color: tomato;
+        svg {
+            fill: tomato;
+        }
+    }
+`;
 
 export default function Layout(){
     return(
@@ -35,7 +68,7 @@ export default function Layout(){
                         </svg>
                     </MenuItem>
                 </Link>
-                <MenuItem>
+                <MenuItem className="log-out">
                     <svg 
                         fill="currentColor" 
                         viewBox="0 0 20 20" 

e3f2f9419000c9df456dbeb9fe4695e358574b94
Rami <138556019+rami242424@users.noreply.github.com>
2025-11-11 11:43:44 +0900

    post tweet form

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,18 +1,75 @@
+import { useState } from "react";
 import styled from "styled-components";
 
-const Form = styled.form``;
-const TextArea = styled.textarea``;
-const AttachFileBtn = styled.label``;
-const AttachFilInput = styled.input``;
-const SubmitBtn = styled.input``;
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`;
+const TextArea = styled.textarea`
+    border-radius: 2px solid white;
+    padding: 20px;
+    border-radius: 20px;
+    font-size: 16px;
+    color: white;
+    background-color: black;
+    width: 100%;
+    resize: none;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    &::placeholder {
+        font-size: 16px;
+    }
+    &:focus {
+        outline: none;
+        border-color: #1d9bf0;
+    }
+`;
+const AttachFileBtn = styled.label`
+    padding: 10px 0px;
+    color: #1d9bf0;
+    text-align: center;
+    border-radius: 20px;
+    border: 1px solid #1d9bf0;
+    font-size: 14px;
+    font-weight: 600;
+    cursor: pointer;
+`;
+const AttachFilInput = styled.input`
+    display : none;
+`;
+const SubmitBtn = styled.input`
+    background-color: #1d9bf0;
+    color: white;
+    border: none;
+    padding: 10px 0px;
+    border-radius: 20px;
+    font-size: 16px;
+    cursor: pointer;
+    &:hover,
+    &:active {
+        opacity: 0.9;
+    }
+`;
 
 export default function PostTweetForm(){
+    const [isLoading, setIsLoading] = useState(false);
+    const [tweet, setTweet] = useState("");
+    const [file, setFile] = useState<File | null>(null);
+    const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setTweet(e.target.value);
+    }
+    const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {files} = e.target;
+        if(files && files.length === 1){
+            setFile(files[0]);
+        }
+    }
     return (
         <Form>
-            <TextArea placeholder="What is happening?"/>
-            <AttachFileBtn htmlFor="file">Add photo</AttachFileBtn>
-            <AttachFilInput id="file" accept="image/*"/>
-            <SubmitBtn />
+            <TextArea rows={5} maxLength={180} onChange={onChange} value={tweet} placeholder="What is happening?"/>
+            <AttachFileBtn htmlFor="file">{file? "Photo Added✅" : "Add photo"}</AttachFileBtn>
+            <AttachFilInput onChange={onFileChange} id="file" type="file" accept="image/*"/>
+            <SubmitBtn type="submit" value={isLoading ? "Posting..." : "Post Tweet"}/>
         </Form>
 );
 }

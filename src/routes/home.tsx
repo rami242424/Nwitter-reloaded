fbbfd8a7be4494f91cc775aced612ef99385c51a
Rami <138556019+rami242424@users.noreply.github.com>
2025-11-12 15:35:40 +0900

    add timeline in home

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import PostTweetForm from "../components/post-tweet-form";
+import Timeline from "../components/timeline";
 
 const Wrapper = styled.div``;
 export default function Home(){
@@ -7,6 +8,7 @@ export default function Home(){
     return(
         <Wrapper>
             <PostTweetForm />
+            <Timeline />
         </Wrapper>
     );
 }

f527a39af6fb6b28c08033e9cc36be0861de3e4e
Rami <138556019+rami242424@users.noreply.github.com>
2025-11-27 17:58:07 +0900

    5.0 User Avatar

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,12 +1,22 @@
-import { auth } from "../firebase"
+import styled from "styled-components";
+import { auth } from "../firebase";
+
+const Wrapper = styled.div``;
+const AvatarUpload = styled.label``;
+const AvatarImg = styled.img``;
+const AvatarInput = styled.input`
+    display: none;
+`;
+const Name = styled.span``;
+
 
 export default function Profile(){
     const user = auth.currentUser;
     return(
         <Wrapper>
-            <AwatarUpload>
+            <AvatarUpload>
                 <AvatarImg />
-            </AwatarUpload>
+            </AvatarUpload>
             <AvatarInput type="file" accept="image/*" />
             <Name>
                 {user?.displayName ? user.displayName : "Anonymous"}
